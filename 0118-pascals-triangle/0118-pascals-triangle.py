ec3657cb6d23a3bcb38245963a1f29f23046a29c
Ahmed Muawia <121751565+Muawia24@users.noreply.github.com>
2025-08-01 14:57:07 +0300

    Time: 0 ms (100%), Space: 17.9 MB (16.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,13 +1,12 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        # Initialize the triangle with 1 values
         triangle = [[1] * i for i in range(1, numRows + 1)]
 
+        # Loop throwgh each row starting from triangle[2]
         for row in range(2, numRows):
+            #loop throw each column of triangle[row]
             for col in range(1, len(triangle[row]) - 1):
                 triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col]
         
         return triangle
-
-
-
-        
