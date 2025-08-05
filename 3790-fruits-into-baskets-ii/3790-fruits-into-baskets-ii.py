88aaec0dfef56d518d7680a3b03d589256d4c580
Ahmed Muawia <121751565+Muawia24@users.noreply.github.com>
2025-08-05 12:10:02 +0300

    Time: 13 ms (88.4%), Space: 18.2 MB (5.55%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        unplaced = 0
+        for i, fruit in enumerate(fruits):
+            for j in range(len(baskets)):
+                if baskets[j] >= fruit:
+                    baskets.pop(j)
+                    break
+        return len(baskets)        
